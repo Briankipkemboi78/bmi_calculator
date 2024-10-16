b65f5978b8ee580f39538ca8a29d776055ecbb00
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-16 14:46:57 +0300

    Adding functionality

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,6 +1,8 @@
-height  = int(input("Enter your height in m: \n"))
+height  = float(input("Enter your height in m: \n"))
 weight = int(input("Enter your weight in kgs: \n"))
 
-bmi = ""
+# BMI is calculated by weight / (height)^2
+
+bmi = round(weight / (height) ** 2, 2)
 
 print(f"Your bmi is {bmi}")
