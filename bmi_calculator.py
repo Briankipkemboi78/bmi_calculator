150772c18969bd140f6e305f0107eeea3b5f8556
Briankipkemboi47 <kipkemboi.brian@yahoo.com>
2024-10-17 09:36:24 +0300

    Adding bmi check

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -3,6 +3,11 @@ weight = int(input("Enter your weight in kgs: \n"))
 
 # BMI is calculated by weight / (height)^2
 
-bmi = round(weight / (height) ** 2, 2)
+bmi = round(weight / (height) ** 2, 1)
 
-print(f"Your bmi is {bmi}") 
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are Underweight")
+elif bmi >= 18.5 and bmi < 25:
+    print(f"Your bmi is {bmi}, You are okay!")
+else:
+    print(f"Your bmi is {bmi}, You are overweight!")
